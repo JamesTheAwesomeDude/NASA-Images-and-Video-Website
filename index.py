3500ee29009fbf3c17083e6985a70ea274582b64
James Edington <codegeek98@gmail.com>
2017-04-29 14:34:43 -0500

    Woorking print thumbnail of 9th image of orange search

--- index.py
+++ index.py
@@ -4,6 +4,8 @@
 import urllib.request
 import urllib.parse
 import json
+import cgi,cgitb
+cgitb.enable()
 
 API_ROOT='https://images-api.nasa.gov'
 API_ENDPOINTS={
@@ -28,3 +30,17 @@ def _url_json(url):
 		return json.loads(
 		 response.read().decode()
 		)
+if __name__=="__main__":
+	x=search_NASA('orange')['collection']['items'][8]
+	print("""Content-type:text/html\r\n\r\n
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>TEST PAGE LMAO</title>
+<body>
+<img src="{src}" title="{title}" />
+</body>
+</html>""".format(
+	 src=[link for link in x['links'] if link['rel']=='preview' and link['render']=='image'][0]['href'],
+	 title=x['data'][0]['title']
+	))
